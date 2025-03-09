1fe701ed9cbb30ef9666261f80d0ef00329e053b
Arnav-NK <143326242+Arnav-NK@users.noreply.github.com>
2025-03-09 21:46:45 +0530

    Delete Backend directory

--- Backend/router/RegisterRouter.js
+++
@@ -1,6 +0,0 @@
-import express from "express";
-import { getRegister, register } from "../controllers/RegisterController.js";
-const router = express.Router();
-router.post("/register", register);
-router.get("/getRegister", getRegister);
-export default router;
