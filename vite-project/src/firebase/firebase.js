88c4aeb4ad6ac96d3179528dccb8e09bda8cb913
Arnav-NK <143326242+Arnav-NK@users.noreply.github.com>
2025-04-08 10:37:29 +0530

    Update firebase.js

--- vite-project/src/firebase/firebase.js
+++ vite-project/src/firebase/firebase.js
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCTW4BNnncklZf6Zch6T2VcQjez8h_z2Rw",
+  apiKey: "",
   authDomain: "job-auto.firebaseapp.com",
   projectId: "job-auto",
   storageBucket: "job-auto.appspot.com",
-  messagingSenderId: "120920153486",
-  appId: "1:120920153486:web:335bfd4d56b9a6f6d318df",
-  measurementId: "G-JTWGE0B8YZ"
+  messagingSenderId: "",
+  appId: "1:xx:web:xx",
+  measurementId: ""
 };
 
 const app = initializeApp(firebaseConfig);
