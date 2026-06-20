6233c907ce5ea1ee27046a781505385145b0f419
Arnav-NK <technicalrustic@gmail.com>
2026-06-20 10:05:38 +0530

    Fix CORS issue by adding vercel production url

--- Backend/app.js
+++ Backend/app.js
@@ -26,6 +26,7 @@ app.use(
       "http://localhost:5173",
       process.env.PORTFOLIO_URL,
       process.env.DASHBOARD_URL,
+      "https://job-application-auto-filler.vercel.app",
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
