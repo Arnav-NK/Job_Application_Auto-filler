3156c8347874ab3d66aedc491894223877e2e65a
Arnav-NK <143326242+Arnav-NK@users.noreply.github.com>
2026-06-20 10:58:41 +0530

    Add files via upload

---
+++ Extension/background.js
@@ -0,0 +1,20 @@
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.action === "fetchData") {
+    // get data of user from backend who latestly parsed the resume
+    fetch("https://job-application-auto-filler.onrender.com/api/v1/getRegister")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Failed to fetch user data");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        sendResponse({ data: data });
+      })
+      .catch((error) => {
+        sendResponse({ error: error.message });
+      });
+
+    return true;
+  }
+});
